6812d4f453cfb83482972956df30a2660ea6340e
ada4424 <ada830318@gmail.com>
2022-12-20 16:11:53 +0900

    hot-first commit

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -8,7 +8,7 @@ public class Demo1Application {
 
     public static void main(String[] args) {
         //main
-        System.out.println();
+        System.out.println("4");
         SpringApplication.run(Demo1Application.class, args);
 
     }
