58413dd43b50ef2bca381aeeb08710949a25d97c
ada4424 <ada830318@gmail.com>
2022-12-20 16:18:55 +0900

    hot-first6 commit

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -9,6 +9,7 @@ public class Demo1Application {
     public static void main(String[] args) {
         //main
         System.out.println("4");
+        System.out.println("6");
         SpringApplication.run(Demo1Application.class, args);
 
     }
